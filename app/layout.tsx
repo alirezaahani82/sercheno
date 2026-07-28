87f39626dab21c1b8cfb1173b911be6225ad362d
alirezaahani82 <alirezaahani375@gmail.com>
2026-07-28 06:06:32 -0700

    Update layout.tsxpopo

--- app/layout.tsx
+++ app/layout.tsx
@@ -63,7 +63,7 @@ export default function RootLayout({
     <html
       lang="fa"
       dir="rtl"
-      className={${geistSans.variable} ${geistMono.variable} h-full antialiased}
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
         {children}
