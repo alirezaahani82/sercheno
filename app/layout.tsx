e44b113dd4c76979c62d4a5e8c5b233d8d564957
alirezaahani82 <alirezaahani375@gmail.com>
2026-07-28 16:00:16 -0700

    Update layout.tsxغاغط

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,9 @@ export const metadata: Metadata = {
   title: "سرچنو",
   description:
     "سرچنو، پلتفرم جست‌وجو و مقایسه مصالح ساختمانی، تجهیزات، فروشندگان، تأمین‌کنندگان و متخصصان صنعت ساختمان.",
+  icons: {
+    icon: "/logo.png",
+  },
 
   keywords: [
     "سرچنو",
