09426c49a52707703555c438c02358636968ca3d
alirezaahani82 <alirezaahani375@gmail.com>
2026-07-28 00:07:10 -0700

    Update page.tsx480

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,1 +1,36 @@
-
+00:04:32.227 Running build in Washington, D.C., USA (East) – iad1
+00:04:32.227 Build machine configuration: 2 cores, 8 GB
+00:04:32.248 Cloning github.com/alirezaahani82/sercheno (Branch: main, Commit: 17037db)
+00:04:32.249 Skipping build cache, deployment was triggered without cache.
+00:04:32.541 Cloning completed: 293.000ms
+00:04:32.829 Running "vercel build"
+00:04:32.942 Vercel CLI 56.5.0
+00:04:33.272 Installing dependencies...
+00:05:04.101 
+00:05:04.101 added 371 packages in 31s
+00:05:04.103 
+00:05:04.103 146 packages are looking for funding
+00:05:04.104   run `npm fund` for details
+00:05:04.149 Detected Next.js version: 16.2.11
+00:05:04.158 Running "npm run build"
+00:05:04.276 
+00:05:04.276 > searchino@0.1.0 build
+00:05:04.276 > next build
+00:05:04.276 
+00:05:04.784   Applying modifyConfig from Vercel
+00:05:04.844 Attention: Next.js now collects completely anonymous telemetry regarding usage.
+00:05:04.844 This information is used to shape Next.js' roadmap and prioritize features.
+00:05:04.844 You can learn more, including how to opt-out if you'd not like to participate in this anonymous program, by visiting the following URL:
+00:05:04.844 https://nextjs.org/telemetry
+00:05:04.844 
+00:05:04.877 ▲ Next.js 16.2.11 (Turbopack)
+00:05:04.880 
+00:05:04.910   Creating an optimized production build ...
+00:05:11.079 ✓ Compiled successfully in 5.8s
+00:05:11.089   Running TypeScript ...
+00:05:15.637 Failed to type check.
+00:05:15.639 
+00:05:15.640 Type error: File '/vercel/path0/app/login/page.tsx' is not a module.
+00:05:15.641 
+00:05:15.675 Next.js build worker exited with code: 1 and signal: null
+00:05:15.724 Error: Command "npm run build" exited with 1
