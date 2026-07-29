0cfdf81b4aa183f7f0b96b65f7e9da5f6568324b
alirezaahani82 <alirezaahani375@gmail.com>
2026-07-29 02:46:04 -0700

    Create page.tsxass

---
+++ app/admin/service/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { useEffect, useState } from "react";
+const fetchProfessionals = async () => {
+  const { data, error } = await supabase
+    .from("professionals")
+    .select("*");
+
+  console.log("ADMIN DATA:", data);
+  console.log("ADMIN ERROR:", error);
+
+  if (error) {
+    alert("خطا: " + error.message);
+    setLoading(false);
+    return;
+  }
+
+  setProfessionals(data || []);
+  setLoading(false);
+};
+useEffect(() => {
+  fetchProfessionals();
+}, []);
