5791a53cb2bde1dc1fa7818e0e28431bffb9fb1c
alirezaahani82 <alirezaahani375@gmail.com>
2026-08-08 05:18:28 -0700

    Update page.tsx

--- app/store/product-register/form/product/page.tsx
+++ app/store/product-register/form/product/page.tsx
@@ -282,7 +282,7 @@ export default function ProductRegisterPage() {
 
             seller_id: storeId,
 
-            status: "approved",
+            status: "inactive",
 
             sales_conditions:
               selectedConditions.length > 0
