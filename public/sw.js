70e17d1d8a33cde7a7e7483e3615e1fc5f80b5a3
alirezaahani82 <alirezaahani375@gmail.com>
2026-08-15 08:12:32 -0700

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "sercheno-v1";
+
+const STATIC_ASSETS = [
+  "/",
+  "/logo.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(STATIC_ASSETS);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
