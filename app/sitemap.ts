5b2eedab40b71d31f819714a74566cf3b9eaa308
alirezaahani82 <alirezaahani375@gmail.com>
2026-09-25 00:34:47 +0330

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -8,24 +8,36 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "weekly",
       priority: 1,
     },
+
+    // صفحه رسمی علیرضا آهنی
+    {
+      url: "https://sercheno-ywf1.vercel.app/alirezaahani",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.9,
+    },
+
     {
       url: "https://sercheno-ywf1.vercel.app/materials",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.9,
     },
+
     {
       url: "https://sercheno-ywf1.vercel.app/service",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.9,
     },
+
     {
       url: "https://sercheno-ywf1.vercel.app/store/register",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.7,
     },
+
     {
       url: "https://sercheno-ywf1.vercel.app/service/register",
       lastModified: new Date(),
@@ -34,4 +46,3 @@ export default function sitemap(): MetadataRoute.Sitemap {
     },
   ];
 }
-
